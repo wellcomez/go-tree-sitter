a3514c3eb65ca9de859b6b12fb2127ecb29b495e
wellcomez <zhu.jialai@gmail.com>
2024-11-05 18:30:18 +0800

    refactor(csharp/swift): update bindings to return unsafe.Pointer
    - Remove sitter package import
    - Comment out GetLanguage functions
    - Add new Language functions that return unsafe.Pointer

--- csharp/binding.go
+++ csharp/binding.go
@@ -7,13 +7,16 @@ import "C"
 import (
 	"unsafe"
 
-	sitter "github.com/smacker/go-tree-sitter"
+	// sitter "github.com/smacker/go-tree-sitter"
 )
 
 // GetLanguage returns a grammar for C# language.
 //
 // Note: The parser is incomplete, it may return a partial or wrong AST! You were warned.
-func GetLanguage() *sitter.Language {
-	ptr := unsafe.Pointer(C.tree_sitter_c_sharp())
-	return sitter.NewLanguage(ptr)
+// func GetLanguage() *sitter.Language {
+// 	ptr := unsafe.Pointer(C.tree_sitter_c_sharp())
+// 	return sitter.NewLanguage(ptr)
+// }
+func Language() unsafe.Pointer{
+	return unsafe.Pointer(C.tree_sitter_c_sharp())
 }
